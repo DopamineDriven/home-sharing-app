6bd9f8d3b003c92228812cdb62f767be6116f24d
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-05-08 16:03:37 -0500

    new build executed; additional commenting on Curl structure

--- server/src/server.ts
+++ server/src/server.ts
@@ -40,4 +40,8 @@ Can use curl to delete a listing
 curl -X POST http://localhost:3002/delete-listing \
     -H 'Content-Type: application/json' \
     -d '{"id":"001"}'
+
+- X specifies which HTTP method to use followed by url
+- H sets Header for http request
+- d sets data to pass (body)
 */
