c3cdfd037f887a04ce6394c8404450b3cc17a7fe
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-05-08 13:37:16 -0500

    using ts types

--- server.ts
+++ server.ts
@@ -9,5 +9,12 @@ app.use(
     express.urlencoded({ extended: true })    
 );
 
+const a: number = 1.5;
+const b: number = 3.14;
+
+app.get("/", (req, res) => res.send(`1.5*3.14=${a*b}`));
+
 // invoke server
 app.listen(PORT)
+
+console.log(`[app]: http://localhost:${PORT}`);
