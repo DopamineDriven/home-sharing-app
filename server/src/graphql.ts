9e31603dbe055b5379703179ba20061844247b7f
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-05-09 19:53:02 -0500

    building Listing type as a GraphQL Object Type

--- server/src/graphql.ts
+++ server/src/graphql.ts
@@ -1,4 +1,12 @@
-import { GraphQLSchema, GraphQLObjectType, GraphQLString } from "graphql";
+import { GraphQLString } from 'graphql';
+import { 
+    GraphQLSchema, 
+    GraphQLObjectType, 
+    GraphQLString,
+    GraphQLID,
+    GraphQLInt,
+    GraphQLFloat 
+} from "graphql";
 
 const query = new GraphQLObjectType({
     name: "Query",
@@ -19,6 +27,16 @@ const mutation = new GraphQLObjectType({
         }
     }
 });
+
+const Listing = new GraphQLObjectType({
+    name: "Listing",
+    fields: {
+        id: { type: GraphQLID },
+        title: { type: GraphQLString },
+        image: { type: GraphQLString },
+        address: { type: GraphQLString },
+    }
+})
 
 export const schema = new GraphQLSchema({
     query,
