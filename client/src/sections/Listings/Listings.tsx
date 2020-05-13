56ca99c4c957f9e686928aec213fa0d2cfd2a19e
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-05-13 01:15:47 -0500

    connecting client and server successful

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React from "react";
 import { server } from "../../lib/api";
 
+// listings var in typescript code, below is query keyword
+// query Listings is for the sake of formality
+const LISTINGS = `
+query Listings {
+    listings {
+        id
+        title
+        image
+        address
+        price
+        numOfGuests
+        numOfBeds
+        numOfBaths
+        rating
+    }
+}
+`;
+
 interface Props {
-    title: string|object
-};
+	title: string | object;
+}
 
 export const Listings = ({ title }: Props) => {
-    const fetchListings = () => {}
-    return (
-        <div>
-            <h2>
-                {title}
-            </h2>
-            <button 
-                onClick={fetchListings}
-                className="btn btn-dark bg-white text-dark btn-lg"
-            >
-                Query Listings
-            </button>
-        </div>
-    )
+	const fetchListings = async () => {
+		const { data } = await server.fetch({ query: LISTINGS });
+		console.log(data);
+	};
+	return (
+		<div>
+			<h2>{title}</h2>
+			<button
+				onClick={fetchListings}
+				className="btn btn-dark bg-white text-dark btn-lg ml-5"
+			>
+				Query Listings
+			</button>
+		</div>
+	);
 };
