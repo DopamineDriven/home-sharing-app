402204c0f89d9112a7947903f4509bdd570712e6
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-05-18 02:07:05 -0500

    interface updates complete

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -2,22 +2,54 @@ import { Collection, ObjectId } from "mongodb";
 
 export interface Booking {
     _id: ObjectId;
+    listing: ObjectId;
+    tenant: string;
+    checkIn: string;
+    checkOut: string;
+}
+
+// bool values indicate which days in the month have been booked
+export interface BookingsIndexMonth {
+    [key: string]: boolean;
+}
+export interface BookingsIndexYear {
+    [key: string]: BookingsIndexMonth;
+}
+export interface BookingsIndex {
+    [key: string]: BookingsIndexYear;
+}
+
+export enum ListingType {
+    Apartment = "APARTMENT",
+    House = "HOUSE"
 }
 
 export interface Listing {
     _id: ObjectId;
-    // title: string;
-    // image: string;
-    // address: string;
-    // price: number;
-    // numOfGuests: number;
-    // numOfBeds: number;
-    // numOfBaths: number;
-    // rating: number;
+    title: string;
+    description: string;
+    image: string;
+    host: string;
+    type: ListingType;
+    address: string;
+    country: string;
+    admin: string;
+    city: string;
+    bookings:  ObjectId[];
+    bookingsIndex: BookingsIndex;
+    price: number;
 }
 
 export interface User {
-    _id: ObjectId;
+    _id: string;
+    token: string;
+    name: string;
+    avatar: string;
+    contact: string;
+    walletId?: string;
+    income: number;
+    bookings: ObjectId[];
+    listings: ObjectId[];
 }
 export interface Database {
     listings: Collection<Listing>;
