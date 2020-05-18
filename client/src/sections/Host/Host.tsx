914a01822c4078e41d1576895efa70cf3155f7b3
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-05-18 00:45:35 -0500

    added 5 new section folders containing an index and a respective page component each

---
+++ client/src/sections/Host/Host.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Host = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+};
--- client/src/sections/index.tsx
+++ client/src/sections/index.tsx
@@ -1,1 +1,6 @@
+export * from "./Home/index";
+export * from "./Host/index";
+export * from "./Listing/index";
 export * from "./Listings/index";
+export * from "./NotFound/index";
+export * from "./User/index";
