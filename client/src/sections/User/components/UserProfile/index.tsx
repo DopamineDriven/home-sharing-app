50028c281edef6ebe11794ef64d793833329b071
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-05-21 07:17:05 -0500

    finished UserProfile child component

--- client/src/sections/User/components/UserProfile/index.tsx
+++ client/src/sections/User/components/UserProfile/index.tsx
@@ -1,13 +1,34 @@
 import React from "react";
 import { User as UserData } from "../../../../lib/graphql/queries/User/__generated__/User";
+import { Avatar, Card, Divider, Typography } from "antd";
 
 // (a)
 interface Props {
     user: UserData["user"];
 }
 
+const { Paragraph, Text, Title } = Typography;
+
 export const UserProfile = ({ user }: Props) => {
-    return <h2>UserProfile</h2>;
+    return (
+        <div className="user-profile">
+            <Card className="user-profile__card">
+                <div className="user-profile__avatar">
+                    <Avatar size={100} src={user.avatar} />
+                </div>
+                <Divider />
+                <div className="user-profile__details">
+                    <Title level={4}>Details</Title>
+                    <Paragraph>
+                        Name: <Text strong>{user.name}</Text>
+                    </Paragraph>
+                    <Paragraph>
+                        Contact: <Text strong>{user.contact}</Text>
+                    </Paragraph>
+                </div>
+            </Card>
+        </div>
+    );
 };
 
 /*
