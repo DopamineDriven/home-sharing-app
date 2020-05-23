918e8a54a293ae26b13719df3966861372a27d25
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-05-22 23:19:50 -0500

    Listing resolver functions continued

--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -1,8 +1,12 @@
 import { IResolvers } from 'apollo-server-express';
 import { ObjectId } from "mongodb";
-import { Database, Listing } from "../../../lib/types";
+import { Database, Listing, User } from "../../../lib/types";
 import { ListingArgs } from "./types";
+import { Request } from "express";
+import { authorize } from "../../../lib/utils";
 
+
+// host field within listing doc obj is an id of user who owns the listing
 export const listingResolvers: IResolvers = {
     Query: {
         listing: async (
@@ -11,12 +15,17 @@ export const listingResolvers: IResolvers = {
             { db, req }: { db: Database; req: Request }
         ): Promise<Listing> => {
             try {
-                const listing = await db.listings.findOne({ _id: new ObjectId });
+                const listing = await db.listings.findOne({ _id: new ObjectId(id) });
                 if (!listing) {
                     throw new Error("listing cannot be found");
                 }
+
+                const viewer = await authorize(db, req);
+                if (viewer && viewer._id === listing.host) {
+                    listing.authorized = true;
+                }
+
                 return listing;
-                
             }catch (error) {
                 throw new Error(`failed to query listing: ${error}`);
             }
@@ -25,6 +34,17 @@ export const listingResolvers: IResolvers = {
     Listing: {
         id: (listing: Listing): string => {
             return listing._id.toHexString();
+        },
+        host: async (
+            listing: Listing,
+            _args: {},
+            { db }: { db: Database }
+        ): Promise<User> => {
+            const host = await db.users.findOne({ _id: listing.host });
+            if (!host) {
+                throw new Error("host cannot be found");
+            }
+            return host;
         }
     }
 };
