bc9f2beea3846379d44a55d52939f475b8d98565
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-05-22 23:32:23 -0500

    ListingBooking Args and Data interfaces in types.ts

--- server/src/graphql/resolvers/Listing/types.ts
+++ server/src/graphql/resolvers/Listing/types.ts
@@ -1,3 +1,28 @@
+import { Booking } from "../../../lib/types";
+
 export interface ListingArgs {
     id: string;
 }
+
+export interface ListingBookingsArgs {
+    limit: number;
+    page: number;
+}
+
+export interface ListingBookingsData {
+    total: number;
+    result: Booking[];
+}
+
+/*
+import Booking interface (descriptive of booking doc shape in database)
+
+export interface Booking {
+    _id: ObjectId;
+    listing: ObjectId;
+    tenant: string;
+    checkIn: string;
+    checkOut: string;
+}
+
+*/
