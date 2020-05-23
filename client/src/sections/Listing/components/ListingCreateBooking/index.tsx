9781e61cd82d8881af054bdf8ca7a722ec5bd2b4
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-05-23 06:15:21 -0500

    had to revert to moment 2.25.3 for DatePicker antd component to function properly; ListingCreateBooking success

--- client/src/sections/Listing/components/ListingCreateBooking/index.tsx
+++ client/src/sections/Listing/components/ListingCreateBooking/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, Card, DatePicker, Divider, Typography } from "antd";
-import { Moment } from "moment";
-import { formatListingPrice } from "../../../../lib/utils";
+import moment, { Moment } from 'moment';
+import { displayErrorMessage, formatListingPrice } from "../../../../lib/utils";
 
 const { Paragraph, Title } = Typography;
 
@@ -10,7 +10,7 @@ interface Props {
     checkInDate: Moment | null;
     checkOutDate: Moment | null;
     setCheckInDate: (checkInDate: Moment | null) => void;
-    setCheckOutDate: (checkOutDate: Moment | null) => void;
+    setCheckOutDate: (checkOutDate: Moment | null) => (void | null);
 }
 
 export const ListingCreateBooking = ({ 
@@ -20,6 +20,30 @@ export const ListingCreateBooking = ({
     setCheckInDate,
     setCheckOutDate 
 }: Props) => {
+    const disabledDate = (currentDate?: Moment | any) => {
+        if (currentDate) {
+            const dateIsBeforeEndOfDay = currentDate.isBefore(moment().endOf("day"));
+
+            return dateIsBeforeEndOfDay;
+        } else {
+            return false;
+        }
+    };
+
+    const verifyAndSelectCheckOutDate = (selectedCheckOutDate: Moment | null) => {
+        if (checkInDate && selectedCheckOutDate) {
+            if (moment(selectedCheckOutDate).isBefore(checkInDate, "days")) {
+                return displayErrorMessage(
+                    "check out date must be on or after the check in date"
+                );
+            }   
+        }
+        setCheckOutDate(selectedCheckOutDate);
+    };
+
+    const checkOutInputDisabled = !checkInDate;
+    const buttonDisabled = !checkInDate || !checkOutDate;
+
     return (
         <div className="listing-booking">
             <Card className="listing-booking__card">
@@ -32,15 +56,34 @@ export const ListingCreateBooking = ({
                     <Divider />
                     <div className="listing-booking__card-date-picker">
                         <Paragraph strong>Check In</Paragraph>
-                        <DatePicker />
+                        <DatePicker 
+                            value={checkInDate ? checkInDate : undefined}
+                            format={"YYYY/MM/DD"}
+                            disabledDate={disabledDate}
+                            showToday={false}
+                            onOpenChange={() => setCheckOutDate(null)}
+                            onChange={dateValue => setCheckInDate(dateValue)} 
+                        />
                     </div>
                     <div className="listing-booking__card-date-picker">
                         <Paragraph strong>Check Out</Paragraph>
-                        <DatePicker />
+                        <DatePicker 
+                            value={checkOutDate ? checkOutDate : undefined}
+                            format={"YYYY/MM/DD"}
+                            disabledDate={disabledDate}
+                            disabled={checkOutInputDisabled}
+                            showToday={false}
+                            onChange={dateValue => verifyAndSelectCheckOutDate(dateValue)}
+                        />
                     </div>
                 </div>
                 <Divider />
-                <Button size="large" type="primary" className="listing-booking__card-cta">
+                <Button 
+                    size="large" 
+                    type="primary" 
+                    className="listing-booking__card-cta"
+                    disabled={buttonDisabled}
+                >
                     Request to book
                 </Button>
             </Card>
