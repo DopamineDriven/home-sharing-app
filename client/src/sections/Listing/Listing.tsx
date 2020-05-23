10bae416215f0ce9ccefab49b83632d156e79d43
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-05-23 01:28:01 -0500

    ListingDetails child component incorporated in parent

--- client/src/sections/Listing/Listing.tsx
+++ client/src/sections/Listing/Listing.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { useQuery } from "@apollo/react-hooks";
-import { Layout } from "antd";
+import { Col, Layout, Row } from "antd";
+import { ListingDetails } from "./components";
 import { ErrorBanner, PageSkeleton } from "../../lib/components";
 import { LISTING } from "../../lib/graphql/queries";
 import { 
@@ -29,6 +30,7 @@ export const Listing = ({ match }: RouteComponentProps<MatchParams>) => {
 
     const listing = data ? data.listing : null;
     const listingBookings = listing ? listing.bookings : null;
+    const listingDetailsElement = listing ? <ListingDetails listing={listing} /> : null;
 
     return loading ? (
         <Content className="listings">
@@ -40,6 +42,12 @@ export const Listing = ({ match }: RouteComponentProps<MatchParams>) => {
             <PageSkeleton />
         </Content>
 	) : (
-        <h2>Listing</h2>
+        <Content className="listings">
+            <Row gutter={24} justify="space-between">
+                <Col xs={24} lg={14} flex="auto">
+                    {listingDetailsElement}
+                </Col>
+            </Row>
+        </Content>
     )
 };
---
+++ client/src/sections/Listing/components/ListingDetails/index.tsx
@@ -0,0 +1,123 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Avatar, Divider, Tag, Typography } from "antd";
+import { EnvironmentOutlined } from "@ant-design/icons";
+import { Listing as ListingData } from "../../../../lib/graphql/queries/Listing/__generated__/Listing";
+import { iconColor } from "../../../../lib/utils";
+
+// (a)
+interface Props {
+	listing: ListingData["listing"];
+}
+
+const { Paragraph, Title } = Typography;
+
+export const ListingDetails = ({ listing }: Props) => {
+	const {
+		title,
+		description,
+		image,
+		type,
+		address,
+		city,
+		numOfGuests,
+		host
+    } = listing;
+    
+    return (
+        <div className="listing-details">
+            <div 
+                style={{ backgroundImage: `url(${image})` }}
+                className="listing-details__image" 
+            />
+            <div className="listing-details__information">
+                <Paragraph
+                    className="listing-details__city-address"
+                    type="secondary"
+                    ellipsis
+                >
+                    <Link to={`/listings/${city}`}>
+                        <EnvironmentOutlined style={{ color: iconColor }} />
+                        {city}
+                    </Link>
+                    <Divider type="vertical" />
+                    {address}
+                </Paragraph>
+                <Title level={3} className="listing-details__title">
+                    {title}
+                </Title>
+            </div>
+
+            <Divider />
+
+            <div className="listing-details__section">
+                <Link to={`/user/${host.id}`}>
+                    <Avatar src={host.avatar} size={64} />
+                    <Title level={2} className="listing-details__host-name">
+                        {host.name}
+                    </Title>
+                </Link>
+            </div>
+
+            <Divider />
+
+            <div className="listing-details__section">
+                <Title level={4}>
+                    About this space
+                </Title>
+                <div className="listing-details__about-items">
+                    <Tag color="magenta">{type}</Tag>
+                    <Tag color="magenta">{numOfGuests} Guests</Tag>
+                </div>
+                <Paragraph ellipsis={{ rows: 3, expandable: true }}>
+                    {description}
+                </Paragraph>
+            </div>
+        </div>
+    );
+};
+
+/*
+(a)
+type of listing prop -> type of listing obj from GQL data returned
+TS's capability of indexed access types (lookup tyoes) achieves this
+*/
+
+
+/*
+export const LISTING = gql`
+    query Listing($id: ID!, $bookingsPage: Int!, $limit: Int!) {
+        listing(id: $id) {
+            id
+            title
+            description
+            image
+            host {
+                id
+                name
+                avatar
+                hasWallet
+            }
+            type
+            address
+            city
+            bookings(limit: $limit, page: $bookingsPage) {
+                total
+                result {
+                    id
+                    tenant {
+                        id
+                        name
+                        avatar
+                    }
+                    checkIn
+                    checkOut
+                }
+            }
+            bookingsIndex
+            price
+            numOfGuests
+        }
+    }
+`;
+*/
