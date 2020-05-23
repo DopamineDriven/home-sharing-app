c2ff3532b2df3ff13931c91bc72f4608f9e5c0be
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-05-23 00:34:05 -0500

    setting up Listing.tsx in client/src/sections

--- client/src/sections/Listing/Listing.tsx
+++ client/src/sections/Listing/Listing.tsx
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useState } from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
+import { Layout } from "antd";
+import { PageSkeleton } from "../../lib/components";
+import { LISTING } from "../../lib/graphql/queries";
+import { 
+    Listing as ListingData,
+    ListingVariables
+} from "../../lib/graphql/queries/Listing/__generated__/Listing";
 
-export const Listing = () => {
+interface MatchParams {
+    id: string;
+}
+
+const PAGE_LIMIT = 3;
+
+export const Listing = ({ match }: RouteComponentProps<MatchParams>) => {
+    const [bookingsPage, setBookingsPage] = useState(1);
+
+    const { data, loading, error } = useQuery<ListingData, ListingVariables>(LISTING, {
+        variables: {
+            id: match.params.id,
+            bookingsPage,
+            limit: PAGE_LIMIT
+        }
+    });
+
+
     return (
         <h2>Listing</h2>
     )
