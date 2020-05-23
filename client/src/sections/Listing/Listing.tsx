ff5a8c6aa978352c5879d688f418d66c42e4b58f
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-05-23 03:21:18 -0500

    Listing page functions as intended in browser

--- client/src/sections/Listing/Listing.tsx
+++ client/src/sections/Listing/Listing.tsx
@@ -64,3 +64,62 @@ export const Listing = ({ match }: RouteComponentProps<MatchParams>) => {
         </Content>
     )
 };
+
+// data to test listingbookingselement on return in dev
+// const listingBookings = {
+//     total: 4,
+//     result: [
+//       {
+//         id: "5daa530eefc64b001767247c",
+//         tenant: {
+//           id: "117422637055829818290",
+//           name: "User X",
+//           avatar:
+//             "https://lh3.googleusercontent.com/a-/AAuE7mBL9NpzsFA6mGSC8xIIJfeK4oTeOJpYvL-gAyaB=s100",
+//           __typename: "User"
+//         },
+//         checkIn: "2019-10-29",
+//         checkOut: "2019-10-31",
+//         __typename: "Booking"
+//       },
+//       {
+//         id: "5daa530eefc64b001767247d",
+//         tenant: {
+//           id: "117422637055829818290",
+//           name: "User X",
+//           avatar:
+//             "https://lh3.googleusercontent.com/a-/AAuE7mBL9NpzsFA6mGSC8xIIJfeK4oTeOJpYvL-gAyaB=s100",
+//           __typename: "User"
+//         },
+//         checkIn: "2019-11-01",
+//         checkOut: "2019-11-03",
+//         __typename: "Booking"
+//       },
+//       {
+//         id: "5daa530eefc64b001767247g",
+//         tenant: {
+//           id: "117422637055829818290",
+//           name: "User X",
+//           avatar:
+//             "https://lh3.googleusercontent.com/a-/AAuE7mBL9NpzsFA6mGSC8xIIJfeK4oTeOJpYvL-gAyaB=s100",
+//           __typename: "User"
+//         },
+//         checkIn: "2019-11-05",
+//         checkOut: "2019-11-09",
+//         __typename: "Booking"
+//       },
+//       {
+//         id: "5daa530eefc64b001767247f",
+//         tenant: {
+//           id: "117422637055829818290",
+//           name: "User X",
+//           avatar:
+//             "https://lh3.googleusercontent.com/a-/AAuE7mBL9NpzsFA6mGSC8xIIJfeK4oTeOJpYvL-gAyaB=s100",
+//           __typename: "User"
+//         },
+//         checkIn: "2019-11-10",
+//         checkOut: "2019-11-11",
+//         __typename: "Booking"
+//       }
+//     ]
+//   } as any;
