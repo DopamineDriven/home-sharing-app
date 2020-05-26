93ff8e5fc96fa372a225bdb9d526c7cee0f57034
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-05-26 02:36:07 -0500

    resolved two reqs being made on searchbar nav from page 2 or greater in listings; useRef and useEffect in ./client/src/sections/Listings/index.tsx via Apollo's skip prop

--- client/src/sections/Listings/index.tsx
+++ client/src/sections/Listings/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link, RouteComponentProps } from "react-router-dom";
 import { useQuery } from "@apollo/react-hooks";
 import { Affix, Layout, List, Typography } from "antd";
@@ -23,10 +23,13 @@ const { Content } = Layout;
 const { PRICE_LOW_TO_HIGH } = ListingsFilter; //enum
 
 export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
+    // (a)
+    const locationRef = useRef(match.params.location);
     const [filter, setFilter] = useState(PRICE_LOW_TO_HIGH);
     const [page, setPage] = useState(1);
 
     const { data, error, loading } = useQuery<ListingsData, ListingsVariables>(LISTINGS, {
+        skip: locationRef.current !== match.params.location && page !== 1,
         variables: {
             location: match.params.location,
             filter, // obj shorthand syntax (OSS)
@@ -34,6 +37,11 @@ export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
             page // OSS
         }
     });
+    // effect runs on location param change
+    useEffect(() => {
+        setPage(1);
+        locationRef.current = match.params.location;
+    }, [match.params.location]);
 
     const listings = data ? data.listings : null;
     const listingsRegion = listings ? listings.region : null;
@@ -104,3 +112,25 @@ export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
 
 // Affix persists Filter and pagination options on scroll,
 // anchoring child props to top of screen (64->64px) 
+
+/*
+(a)
+    adding useRef after establishing useEffect hook necessary
+    why?
+        Two network requests were being made on change despite the fact that
+        navigating from page 2 of listings to another location returned page 1
+        as intended
+        that is, variables in queries are being updated twice
+            (1) location changes (query)
+            (2) then, effect runs and page reset to one for new location
+    useRef -> skip first request under condition that page is updated back to 1
+    via useEffect; that is, skip initial location change network request
+    How to achieve?
+        Via Apollo's skip property -> skip:boolean
+            if skip is true, query will be skipped entirely
+            https://www.apollographql.com/docs/react/api/react-apollo/
+        useRef keeps context of location
+    The end result
+        network req is skipped if locationRef.current !== current location && page !== 1
+        locationRef.current passed in useEffect hook to update current location each time
+*/
