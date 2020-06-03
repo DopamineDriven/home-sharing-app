a68bf5cd2aa524ae7d7813edee5205956ed1292c
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-06-02 23:07:38 -0500

    notfound page updated

--- client/src/sections/NotFound/NotFound.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const NotFound = () => {
-    return (
-        <h2>Not Found</h2>
-    )
-};
--- client/src/sections/NotFound/index.tsx
+++ client/src/sections/NotFound/index.tsx
@@ -1,1 +1,31 @@
-export * from "./NotFound";
+import React, { Fragment } from 'react';
+import { Link } from "react-router-dom";
+import { Empty, Layout, Typography } from "antd";
+
+const { Content } = Layout;
+const { Text } = Typography;
+
+export const NotFound = () => {
+    return (
+        <Content className="not-found">
+            <Empty 
+                description={
+                    <Fragment>
+                        <Text className="not-found__description-title">
+                            Uh Oh! Something went wrong
+                        </Text>
+                        <Text className="not-found__description-subtitle">
+                            Page searched for cannot be found
+                        </Text>
+                    </Fragment>
+                }
+            />
+            <Link 
+                to="/"
+                className="not-found__cta ant-btn ant-btn-primary ant-btn-lg"
+            >
+                Return Home
+            </Link>
+        </Content>
+    );
+};
